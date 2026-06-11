47380f33c271e93386404b734d20690f92b52141
DELL <balqeesjamil7@gmail.com>
2026-06-12 01:49:25 +0300

    Add Arabic label mapping for sign translations

---
+++ arabic_mapping.py
@@ -0,0 +1,67 @@
+ARABIC_MAP = {
+
+    "Animals": "حيوانات",
+
+    "Attentive": "مُتيقظ",
+
+    "brother": "أخ",
+
+    "builder": "بناء",
+
+    "cheap": "رخيص",
+
+    "Doctor": "دكتور",
+
+    "excellent": "بارع",
+
+    "friend": "صديق",
+
+    "good": "رائع",
+
+    "him": "هو",
+
+    "how": "كيف",
+
+    "husband": "شريك",
+
+    "mechanic": "ميكانيكي",
+
+    "morning": "صباح",
+
+    "plane": "طائرة",
+
+    "president": "رئيس",
+
+    "satisfaction": "الاكتفاء",
+
+    "school": "مدرسة",
+
+    "six": "ستة",
+
+    "seven": "سبعة",
+
+    "teacher": "أستاذ",
+
+    "translator": "مترجم",
+
+    "vegetables": "خضروات",
+
+    "very good": "جيد جداً",
+
+    "wait": "انتظر",
+
+    "wash": "نظف",
+
+    "watch maker": "مصلح الساعات",
+
+    "what": "ماذا",
+
+    "when": "متى",
+
+    "who": "من",
+
+    "with_god_will": "إن شاء الله",
+
+    "yours": "لك"
+
+}
